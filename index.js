f396c59124de124177399c3ef4e18efb61b4a822
Dhavalbhatt29 <dhavalbhatt246@gmail.com>
2025-05-06 10:38:53 +0530

    First backend

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const cors = require("cors")
+const serverless = require("serverless-http")
+const app = express()
+app.use(cors())
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send("success");
+})
+
+app.listen(5000, () => {
+    console.log("Server is running on the port 5000")
+})
+
+module.exports.handler = serverless(app);
